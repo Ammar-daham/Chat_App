2acaaa699ca467572f3a18edf61be168763624e3
Ammar Daham <aljewaryammar@gmail.com>
2024-08-04 11:17:00 +0300

    client init

---
+++ src/chatapp/client/Client.java
@@ -0,0 +1,91 @@
+package chatapp.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client implements Runnable
+{
+	
+	Socket client;
+	private BufferedReader in;
+	private PrintWriter out;
+	private boolean done;
+	
+	@Override
+	public void run()
+	{
+		try
+		{
+			client = new Socket("127.0.0.1", 9999);
+			out = new PrintWriter(client.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			
+			InputHandler inHandler = new InputHandler();
+			Thread t = new Thread(inHandler);
+			t.start();
+			
+			String inMessage;
+			while ((inMessage = in.readLine()) != null)
+			{
+				System.out.println(inMessage);
+			}
+		}
+		catch (IOException e)
+		{
+			shutdown();
+		}
+	}
+	
+	public void shutdown()
+	{
+		done = true;
+		try
+		{
+			in.close();
+			out.close();
+			if (!client.isClosed())
+			{
+				client.close();
+			}
+		}
+		catch (IOException e)
+		{
+			// ignore
+		}
+	}
+	
+	class InputHandler implements Runnable
+	{
+		
+		@Override
+		public void run()
+		{
+			try
+			{
+				BufferedReader inReeder = new BufferedReader(new InputStreamReader(System.in));
+				while (!done)
+				{
+					String message = inReeder.readLine();
+					if (message.equals("/quit"))
+					{
+						inReeder.close();
+						shutdown();
+					}
+					else
+					{
+						out.println(message);
+					}
+				}
+			}
+			catch (IOException e)
+			{
+				shutdown();
+			}
+		}
+		
+	}
+	
+}
